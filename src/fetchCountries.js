b7775855c09d183d61c53a33ffb6fe37f0d63f0b
Kristuwa <72200499+Kristuwa@users.noreply.github.com>
2022-09-26 23:54:02 +0300

    add files

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,7 +1,15 @@
-export function fetchCountries(name) {
-  return fetch(`https://restcountries.com/v3.1/name/${name}`)
-    .then(response => {
-      return response.json;
-    })
-    .then(countries => console.log(countries));
+import Notiflix from 'notiflix';
+
+export default function fetchCountries(name) {
+  return fetch(
+    //  `https://restcountries.eu/rest/v2/all?fields=${name};capital;flag;population;languages`
+    `https://restcountries.com/v3.1/name/${name}?fields=name,capital,flags,population,languages,
+	 `
+  ).then(response => {
+    if (!response.ok) {
+      Notiflix.Notify.failure('"Oops, there is no country with that name"');
+      throw new Error(response.status);
+    }
+    return response.json();
+  });
 }
